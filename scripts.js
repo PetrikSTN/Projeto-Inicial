a269be9908323a4cc0ec2c23ceb5f9be004f3fe2
Petrik Henrique de Jesus dos Santos <rm90002@estudante.fieb.edu.br>
2024-04-03 09:48:37 -0300

    Consulta via Fetch

--- scripts.js
+++ scripts.js
@@ -4,7 +4,16 @@ botao.addEventListener("click", alertar,false);
     var nome = document.getElementById("nome");
     var email = document.getElementById("email");
     var saida = document.getElementById("saida-de-dados");
-    var email = document.getElementById("cep");
+    var cep = document.getElementById("cep");
+    var telefone = document.getElementById("telefone");
+    var complemento = document.getElementById("complemento");
+    var logradouro = document.getElementById("logradouro");
+    var bairro = document.getElementById("bairro");
+    var cidade = document.getElementById("cidade");
+    var estado = document.getElementById("estado");
+
+
+
 
 function alertar(event){
    // alert("Voce clicou no botao!!!" + "" + nome.value);
@@ -14,13 +23,33 @@ function alertar(event){
    // if(resultado == 0){
    //     alert("este numero é par!");
   //  }
+
+  const url = `https://viacep.com.br/ws/${cep.value}/json`;
+
+  fetch(url)
+  .then(resposta=> {
+return resposta.json();
+  }
+  )
+  .then(dadosDoEndereco=>{
+    logradouro.value = dadosDoEndereco.logradouro;
+    bairro.value = dadosDoEndereco.bairro;
+    cidade.value = dadosDoEndereco.localidade;
+    estado.value = dadosDoEndereco.uf;
+    complemento.value = dadosDoEndereco.complemento;
+  })
+  .catch(function(e){
+    alert(e.message());
+  })
+
     saida.innerText = 
     "nome: " + nome.value + 
     "\n Email: " + email.value +
     "\n cep:" + cep.value +
     "\n telefone:" + telefone.value + 
     "\n Complemento:" + complemento.value +
+    "\n logradouro" + logradouro.value +
     "\n Bairro:" +  bairro.value +  
-    "\n Cidade:" + Cidade.value; +  
-    "\n Estado:"+ Estado.value
+    "\n Cidade:" + cidade.value +  
+    "\n Estado:"+ estado.value;
 }
